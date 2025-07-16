3ca8e75595831e93a2b214fda916fb4664571062
Lisa Austin <lisarachelle85@gmail.com>
2025-07-16 09:30:31 -0400

    Updated Script index

--- frontend/config.js
+++ frontend/config.js
@@ -12,6 +12,7 @@
 //const BASE_API = "http://10.0.0.220:3000";
 //const BASE_UPLOAD = "http://10.0.0.220:3001";
 
+// LIVE DEPLOYED SERVERS
 const BASE_API = "https://achievement-backend-a693.onrender.com";
 const BASE_UPLOAD = "https://achievement-backend-a693.onrender.com";
 
